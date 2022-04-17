adc9763bf2d903642262f63ed4783f320b8ad1d5
mraware <aaronware14@gmail.com>
2022-04-17 13:26:50 -0500

    patchie

--- omegga.plugin.js
+++ omegga.plugin.js
@@ -1,18 +1,16 @@
-class BuildingMirror {
+class BuildingCircle {
   constructor(omegga, config, store) {
     this.omegga = omegga;
     this.config = config;
     this.store = store;
-
-    this.playerData = {};
   }
 
   async init() {
     this.omegga
-      .on('cmd:mirror', this.mirror);
+      .on('cmd:circle', this.circle)
 
     return {
-      registeredCommands: ['mirror']
+      registeredCommands: ['circle']
     };
   }
 
@@ -20,7 +18,9 @@ class BuildingMirror {
     const player = this.omegga.getPlayer(senderName);
     if (
       this.config['only-authorized'] && !player.isHost() &&
-      !this.config['authorized-users'].some(p => player.id === p.id)
+      !this.config['authorized-users'].some(p => {
+        return player.id === p.id
+      })
     ) {
       this.omegga.whisper(senderName, '<color="ff0000">Unauthorized to use command.</>');
       return true;
@@ -28,244 +28,189 @@ class BuildingMirror {
     return false;
   }
 
-  rotate(rotation, turns) {
-    return (rotation + turns) % 4;
-  }
-
-  shouldResize(rotation, rot) {
-    return (rot + rotation) % 2 === 1;
-  }
-
-  convertDirection({ direction, rotation, size }, axis, brickType) {
-    const rotationType = rotationTypes[brickType];
-    const map = axisMap[direction];
-    let dir = direction;
-    let rot = rotation;
-    let scale = size;
-    if (axis[map.axis]) {
-      dir = map.directionFlip;
-      if (rotationType === 1) {
-        rot = this.rotate(rot, rot % 2 ? map.flip ? 0 : 2 : map.flip ? 2 : 0);
-      } else if (rotationType === 2) {
-        rot = this.rotate(rot, rot % 2 ? map.flip ? 1 : 3 : map.flip ? 3 : 1);
-      } else if (rotationType === 3) {
-        rot = this.rotate(rot, rot % 2 ? map.flip ? 3 : 1 : map.flip ? 1 : 3);
-      } else if (rotationType === 4) {
-        rot = this.rotate(rot, rot % 2 ? map.flip ? 2 : 0 : map.flip ? 0 : 2);
+  // Fill holes for non-concave shapes
+  optimize(bricks, fill, brickGenerated) {
+    function combineBricks(first, last) {
+      // same brick
+      if (first.position[0] === last.position[0] && first.position[1] === last.position[1]) {
+        return first
+      }
+      const { center, minBound, maxBound } = global.OMEGGA_UTIL.brick.getBounds({ brick_assets: [brickGenerated.type], bricks: [first, last] });
+      const size = [Math.abs(minBound[0]-maxBound[0])/2, Math.abs(minBound[1]-maxBound[1])/2, Math.abs(minBound[2]-maxBound[2])/2]
+      return {
+        ...first,
+        position: center,
+        size
       }
     }
-    if (axis[map.turn1]) {
-      if (rotationType === 1) {
-        rot = this.rotate(rot, rot % 2 ? 2 : 0);
-      } else if (rotationType === 2) {
-        rot = this.rotate(rot, rot % 2 ? 3 : 1);
-      } else if (rotationType === 3) {
-        rot = this.rotate(rot, rot % 2 ? 1 : 3);
-      } else if (rotationType === 4) {
-        rot = this.rotate(rot, rot % 2 ? 0 : 2);
-      }
 
-    }
-    if (axis[map.turn3]) {
-      if (rotationType === 1) {
-        rot = this.rotate(rot, rot % 2 ? 0 : 2);
-      } else if (rotationType === 2) {
-        rot = this.rotate(rot, rot % 2 ? 1 : 3);
-      } else if (rotationType === 3) {
-        rot = this.rotate(rot, rot % 2 ? 3 : 1);
-      } else if (rotationType === 4) {
-        rot = this.rotate(rot, rot % 2 ? 2 : 0);
+    function optimizeDirection(bricks, primary, secondary) {
+      const newBricks = [];
+      const sort = bricks.sort((left, right) => left.position[secondary] - right.position[secondary] || left.position[primary] - right.position[primary]);
+      let first = sort[0];
+      let last = sort[0];
+      for (let i = 1; i < sort.length; i++) {
+        const current = sort[i];
+        if (
+          first.size[secondary] === current.size[secondary] &&
+          first.position[secondary] === current.position[secondary]
+        ) {
+          if (fill || (last.position[primary] + last.size[primary]) === (current.position[primary] - current.size[primary])) {
+            last = current;
+          } else {
+            newBricks.push(combineBricks(first, last));
+            first = current;
+            last = current;
+          }
+        } else {
+          newBricks.push(combineBricks(first, last));
+          first = current;
+          last = current;
+        }
       }
+      newBricks.push(combineBricks(first, last));
+
+      return newBricks;
     }
-    if (this.shouldResize(rotation, rot)) {
-      scale = [size[1], size[0], size[2]];
-    }
-    return { direction: dir, rotation: rot, size: scale };
+
+    return optimizeDirection(optimizeDirection(bricks, 0, 1), 1, 0);
   }
 
-  mirror = async (senderName, axisString) => {
+  generateCircle(radius, color, fill, brickGenerated) {
+    const bricks = [];
+
+    function addBrick(x, y) {
+      if (Object.is(x, -0) || Object.is(y, -0))
+        return;
+      bricks.push({
+        color,
+        owner_index: 0,
+        size: brickGenerated.size,
+        position: [ x*brickGenerated.size[0]*2, y*brickGenerated.size[1]*2, brickGenerated.size[2]*2],
+        material_index: 0,
+        direction: 4,
+        rotation: 0
+      })
+    }
+
+    let f = 1- radius;
+    let ddF_x = 0;
+    let ddF_y = -2 * radius;
+    let x = 0;
+    let y = radius;
+
+    addBrick(0, radius)
+    addBrick(0, -radius)
+    addBrick(radius, 0)
+    addBrick(-radius, 0)
+
+    while(x < y) {
+      if (f >= 0) {
+        y--;
+        ddF_y += 2;
+        f += ddF_y;
+      }
+      x++;
+      ddF_x += 2;
+      f += ddF_x + 1;
+
+      if (y<x) {
+        break;
+      }
+
+      addBrick(x, y);
+      addBrick(-x, y);
+      addBrick(x, -y);
+      addBrick(-x, -y);
+
+      if (x != y) {
+        addBrick(y, x);
+        addBrick(-y, x);
+        addBrick(y, -x);
+        addBrick(-y, -x);
+      }
+    }
+
+    return this.optimize(bricks, fill, brickGenerated);
+  }
+
+  circle = async (senderName, radius, fill = 0) => {
     if(this.unauthorized(senderName)) return;
-    if (axisString) {
-      axisString = axisString.toLowerCase();
-      const axis = [axisString.includes('x'), axisString.includes('y'), axisString.includes('z')];
-      if ((axis[0] || axis[1] || axis[2])) {
-        const player = this.omegga.getPlayer(senderName);
-        const nameColor = player.getNameColor();
-        this.omegga.broadcast(`<b><color="${nameColor}">${senderName}</></> mirroring selection (${axis[0] ? 'X' : ''}${axis[1] ? 'Y' : ''}${axis[2] ? 'Z' : ''})...`);
-        let saveData = await player.getTemplateBoundsData();
+    if (radius) {
+      const player = this.omegga.getPlayer(senderName);
+      const nameColor = player.getNameColor();
+      this.omegga.broadcast(`<b><color="${nameColor}">${senderName}</></> creating circle with radius ${radius}...`);
+      const paint = await player.getPaint();
+      const brickGenerated = await this.getGeneratedGhostBrick(player);
 
-        const { maxBound } = global.OMEGGA_UTIL.brick.getBounds(saveData);
-
-        saveData.bricks = saveData.bricks.map((brick) => {
-          const { direction, rotation, size } = this.convertDirection(brick, axis, saveData.brick_assets[brick.asset_name_index]);
-          return {
-            ...brick,
-            position: brick.position.map((val, index) => (axis[index] ? maxBound[index] - val : val)),
-            direction,
-            rotation,
-            size
-          };
-        });
-        saveData = global.OMEGGA_UTIL.brick.setOwnership(player, saveData);
-        await player.loadDataAtGhostBrick(saveData);
-      } else {
-        this.omegga.whisper(senderName, 'Enter a valid axis to mirror over: X, Y, or Z');
+      if (!paint) {
+        this.omegga.whisper(senderName, 'Select a paint color.');
+        return;
       }
+      if (!brickGenerated) {
+        this.omegga.whisper(senderName, 'Missing ghost brick.');
+        return;
+      }
+
+      let saveData = {
+        version: 10,
+        materials: [paint.material],
+        brick_assets: [brickGenerated.type],
+        bricks: this.generateCircle(radius, paint.color, +fill, brickGenerated)
+      };
+      await player.loadSaveData(saveData);
     } else {
-      this.omegga.whisper(senderName, 'Missing mirror axis: X, Y, or Z');
+      this.omegga.whisper(senderName, 'Must enter a radius');
     }
   }
+
+  // getall BrickGridPreviewActor SimpleParameters
+  // [2021.04.12-03.29.40:573][579]0) BrickGridPreviewActor /Game/Maps/Plate/Plate.Plate:PersistentLevel.BrickGridPreviewActor_2147482489.SimpleParameters = (BrickType=(BrickType=BrickTypeGenerated'"/Game/Bricks/Procedural/PB_DefaultBrick.PB_DefaultBrick:BrickTypeGenerated_2147482394"'),BrickColor=(B=255,G=255,R=255,A=255),MaterialAlpha=5,PreviewIgnoredBricks=)
+
+  // GetAll BrickTypeGenerated HalfSize
+  // [2021.04.12-03.51.37:292][ 65]0) BrickTypeGenerated /Game/Bricks/Procedural/PB_DefaultBrick.PB_DefaultBrick:BrickTypeGenerated_2147482394.HalfSize = (X=5,Y=5,Z=12)
+  async getGeneratedGhostBrick({controller}) {
+    const ownerRegExp = /^(?<index>\d+)\) BrickGridPreviewActor (.+):PersistentLevel\.(?<actor>BrickGridPreviewActor_\d+)\.Owner = BP_PlayerController_C'(.+):PersistentLevel\.(?<controller>BP_PlayerController_C_\d+)'$/;
+    const simpleParamsRegExp = /^(?<index>\d+)\) BrickGridPreviewActor (.+):PersistentLevel\.(?<actor>BrickGridPreviewActor_\d+)\.SimpleParameters = \(BrickType=\(.*(\.(?<brickType>.+):(?<brickTypeGenerated>.+)"'|None)\),BrickColor=\(B=(?<b>\d*),G=(?<g>\d*),R=(?<r>\d*),A=(?<a>\d*)\),PreviewIgnoredBricks=.*\)$/;
+    const brickSizeRegExp = /^(?<index>\d+)\) BrickTypeGenerated .+:(?<actor>BrickTypeGenerated_\d+)\.HalfSize = \(X=(?<x>\d*),Y=(?<y>\d*),Z=(?<z>\d*)*\)$/;
+
+    const [ownersMatch, simpleParametersMatch] = await Promise.all([
+      this.omegga.watchLogChunk('GetAll BrickGridPreviewActor Owner', ownerRegExp, {first: 'index', timeoutDelay: 500}),
+      this.omegga.watchLogChunk('GetAll BrickGridPreviewActor SimpleParameters', simpleParamsRegExp, {first: 'index', timeoutDelay: 500}),
+    ]);
+
+
+    // get BrickGridPreviewActor by controller
+    const owner = ownersMatch.find((owner) => owner.groups.controller === controller);
+
+    if (!owner)
+      return;
+
+    const actor = owner.groups.actor;
+    // get transform parameters for the found actor
+    const simpleParameters = simpleParametersMatch.find((simpleParameters) => simpleParameters.groups.actor === actor);
+
+    if (!simpleParameters)
+      return;
+
+    const { brickType, brickTypeGenerated } = simpleParameters.groups;
+
+    const brickSizeMatch = await this.omegga.watchLogChunk(`GetAll BrickTypeGenerated HalfSize Name=${brickTypeGenerated}`, brickSizeRegExp, {first: 'index', timeoutDelay: 500})
+
+    if (!brickSizeMatch || brickSizeMatch.length === 0)
+      return;
+
+    const brickSize = [+brickSizeMatch[0].groups.x, +brickSizeMatch[0].groups.y, +brickSizeMatch[0].groups.z];
+
+
+    return { type: brickType, size: brickSize }
+  }
+
 
   stop() {
     this.omegga
-      .removeListener('cmd:mirror', this.mirror);
+      .removeAllListeners('cmd:circle');
   }
 }
 
-const rotationTypes = {
-  PB_DefaultBrick: 1,
-  PB_DefaultRamp: 1,
-  PB_DefaultRampCrest: 1,
-  PB_DefaultRampCrestCorner: 2,
-  PB_DefaultRampCrestEnd: 4,
-  PB_DefaultRampInnerCorner: 3,
-  PB_DefaultRampInnerCornerInverted: 3,
-  PB_DefaultRampInverted: 1,
-  PB_DefaultRampCorner: 3,
-  PB_DefaultRampCornerInverted: 3,
-  PB_DefaultSideWedge: 3,
-  PB_DefaultTile: 1,
-  PB_DefaultWedge: 1,
-  PB_DefaultSideWedgeTile: 3,
-  PB_DefaultMicroBrick: 1,
-  PB_DefaultMicroWedgeCorner: 3,
-  PB_DefaultMicroWedgeInnerCorner: 3,
-  PB_DefaultMicroWedgeOuterCorner: 3,
-  PB_DefaultMicroWedgeTriangleCorner: 3,
-  PB_DefaultMicroWedge: 3,
-  PB_DefaultStudded: 1,
-  B_1x1_Brick_Side: 4,
-  B_1x1_Brick_Side_Lip: 1,
-  B_1x1_Cone: 0,
-  B_1x1_Round: 0,
-  B_1x1F_Octo: 1,
-  B_1x1F_Round: 0,
-  B_1x2_Overhang: 1,
-  B_1x2f_Plate_Center: 1,
-  B_1x2f_Plate_Center_Inv: 1,
-  B_1x4_Brick_Side: 4,
-  B_1x_Octo: 1,
-  B_1x_Octo_90Deg: 4,
-  B_1x_Octo_90Deg_Inv: 4,
-  B_1x_Octo_T: 4,
-  B_1x_Octo_T_Inv: 4,
-  B_2x1_Slipper: 1,
-  B_2x2_Cone: 0,
-  B_2x2_Corner: 3,
-  B_2x2_Overhang: 1,
-  B_2x2_Round: 0,
-  B_2x2_Slipper: 1,
-  B_2x2F_Octo: 1,
-  B_2x2F_Octo_Converter: 1,
-  B_2x2F_Octo_Converter_Inv: 1,
-  B_2x2f_Plate_Center: 1,
-  B_2x2f_Plate_Center_Inv: 1,
-  B_2x2F_Round: 0,
-  B_2x4_Door_Frame: 1,
-  B_2x_Cube_Side: 0,
-  B_2x_Octo: 1,
-  B_2x_Octo_90Deg: 4,
-  B_2x_Octo_90Deg_Inv: 4,
-  B_2x_Octo_Cone: 0,
-  B_2x_Octo_T: 4,
-  B_2x_Octo_T_Inv: 4,
-  B_4x4_Round: 0,
-  B_8x8_Lattice_Plate: 0,
-  B_Bishop: 0,
-  B_Bone: 2,
-  B_BoneStraight: 1,
-  B_Branch: 1,
-  B_Bush: 0,
-  B_Cauldron: 0,
-  B_Chalice: 0,
-  B_CheckPoint: 1,
-  B_Coffin: 1,
-  B_Leaf_Bush: 1,
-  B_Coffin_Lid: 1,
-  B_Fern: 2,
-  B_Flame: 0,
-  B_Flower: 0,
-  B_Gravestone: 0,
-  B_GoalPoint: 1,
-  B_Handle: 1,
-  B_Hedge_1x1: 1,
-  B_Hedge_1x1_Corner: 3,
-  B_Hedge_1x2: 1,
-  B_Hedge_1x4: 1,
-  B_Inverted_Cone: 0,
-  B_Jar: 0,
-  B_King: 4,
-  B_Knight: 4,
-  B_Ladder: 1,
-  B_Pawn: 0,
-  B_Picket_Fence: 1,
-  B_Pine_Tree: 0,
-  B_Pumpkin: 4,
-  B_Pumpkin_Carved: 4,
-  B_Queen: 4,
-  B_Rook: 4,
-  B_Sausage: 1,
-  B_Small_Flower: 0,
-  B_SpawnPoint: 1,
-  B_Swirl_Plate: 1,
-  B_Turkey_Body: 1,
-  B_Turkey_Leg: 1,
-};
-
-const axisMap = {
-  0: {
-    axis: 0,
-    flip: false,
-    turn1: 1,
-    turn3: 2,
-    directionFlip: 1
-  },
-  1: {
-    axis: 0,
-    flip: false,
-    turn1: 1,
-    turn3: 2,
-    directionFlip: 0
-  },
-  2: {
-    axis: 1,
-    flip: false,
-    turn1: 0,
-    turn3: 2,
-    directionFlip: 3
-  },
-  3: {
-    axis: 1,
-    flip: false,
-    turn1: 0,
-    turn3: 2,
-    directionFlip: 2
-  },
-  4: {
-    axis: 2,
-    flip: true,
-    turn1: 1,
-    turn3: 0,
-    directionFlip: 5
-  },
-  5: {
-    axis: 2,
-    flip: true,
-    turn1: 1,
-    turn3: 0,
-    directionFlip: 4
-  },
-};
-
-
-module.exports = BuildingMirror;
+module.exports = BuildingCircle;
